2daecdd1574df228d871d90c1509ec19c5a1ede0
Mister1SI <wazzup2690@gmail.com>
2024-03-18 20:38:00 -0700

    kinda works

--- src/main.asm
+++ src/main.asm
@@ -5,17 +5,24 @@ extern prints
 _start:
 	
 	; Get argc and print help if it's only 1
-	mov rdi, [rsp + 8]
-	mov rsi, [rsp + 16]
-	mov [argc], rdi
-	mov [argv], rsi
+	pop rdi
+	pop rsi
+	mov qword [argc], rdi
+	mov qword [argv], rsi
 	cmp rdi, 1
 	je .print_help
 
+	mov rdi, [argc]
+	cmp rdi, 2
+	jl .bad
 	; Print the first argument
 	mov rbx, [argv]		; Get the pointer to the first string
-	mov rsi, [rbx]		; Get the start of the first string
-	mov rsi, [rsi]
+	mov rsi, [rsi]		; Get the start of the first string
+	call prints
+	jmp .exit
+
+	.bad:
+	mov rsi, less
 	call prints
 	jmp .exit
 
@@ -34,5 +41,6 @@ section .bss
 	argv resq 1
 
 section .data
-	help_text db "table: file preprocessor", 10, "usage:", 10, "table <file>", 10, 0
+	help_text db "table: file preprocessor", 10, "usage:", 10, 9, "table <file>", 10, 0
+	less db "Less than! Oh no!", 10, 0
 
